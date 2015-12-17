fa9c5b63761b535642d24ab57f224456c29a44fe
Kyle Demeule <kyle.demeule@gmail.com>
2015-12-16 17:28:47 -0800

    Some UI improvements.

--- app/static/app.js
+++ app/static/app.js
@@ -1,12 +1,28 @@
+var add_player = function() {
+    var empty_row = $("#empty-player-row tr:first").html();
+    $('#player-list tr:last').after("<tr>" + empty_row + "</tr>");
+    $('#player-list tr:last input:first').focus();
+};
+
 $(document).ready(function() {
     $("#add-player").click(function() {
-        var empty_row = $("#empty-player-row tr:first").html();
-        $('#player-list tr:last').after("<tr>" + empty_row + "</tr>");
+        add_player();
     });
 
     $("div.main").on("click", "button.remove-player", function() {
         if($("button.remove-player").size() > 2) {
             $(this).closest("tr").remove();
+        }
+    });
+
+    $('#make-roster-form').on('keyup keypress', function(e) {
+        var code = e.keyCode || e.which;
+        if (code == 13) { 
+            e.preventDefault();
+            if(e.type == "keyup") {
+                add_player();
+            }
+            return false;
         }
     });
 })
